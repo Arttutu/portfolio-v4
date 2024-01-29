3e54351ee054fe2b0387c531e04da38067f10e7c
Arttutu <arthurtt08@gmail.com>
2024-01-29 10:38:22 -0300

    update estilo responsivo

--- src/Componentes/Banner/index.jsx
+++ src/Componentes/Banner/index.jsx
@@ -3,7 +3,7 @@ import banner from "./logo-avatar.png"
 export default function Banner() {
   return (
     <img
-      className="w-96 h-96 sm:w-80 sm:h-80 bg-gray-400 rounded-sm "
+      className="w-96 h-96 sm:w-80 sm:h-auto bg-gray-400 rounded-sm "
       src={banner}
       alt="Minha foto pesssoal"
     ></img>
