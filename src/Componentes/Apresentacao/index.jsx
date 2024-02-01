bf7e6de93e9b7f4e8b78299d5d0187f09c0ee519
Arttutu <arthurtt08@gmail.com>
2024-02-01 10:34:16 -0300

    mudando estilo do card, trocando foto do banner

--- src/Componentes/Apresentacao/index.jsx
+++ src/Componentes/Apresentacao/index.jsx
@@ -7,7 +7,7 @@ export default function Apresentacao() {
   return (
     <section className="container mt-14 sm:mt-32 mx-auto">
       <div className=" lg:justify-around flex flex-col-reverse items-center gap-8 lg:flex lg:flex-row">
-        <article className="flex flex-col items-center text-center sm:text-start w-100  sm:w-3/5 sm:items-start">
+        <article className="flex flex-col items-center text-center sm:text-start w-100  sm:w-1/2 sm:items-start">
           <h1 className=" text-white text-4xl  sm:text-7xl font-semibold  after:content-['Arthur.'] after:sm:text-7xl after:text-4xl after:px-5 after:border-b-4 after:border-green-400">
             Prazer em conhecer você ! eu sou
           </h1>
--- src/Componentes/Banner/index.jsx
+++ src/Componentes/Banner/index.jsx
@@ -1,9 +1,9 @@
 import React from "react"
-import banner from "./logo-avatar.png"
+import banner from "./logo_avatar.jpeg"
 export default function Banner() {
   return (
     <img
-      className="w-80  sm:w-96 sm:h-auto bg-gray-400 rounded-lg "
+      className="w-96 h-50 md:w-1/3 md:h-auto sm:w-1/2 bg-gray-400 rounded-lg "
       src={banner}
       alt="Minha foto pesssoal"
     ></img>
