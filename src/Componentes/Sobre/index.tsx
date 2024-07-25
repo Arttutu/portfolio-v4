47004b04a2a17edcdb4e0427c48a5bb8c5ee1542
Arttutu <arthurtt08@gmail.com>
2024-07-25 10:40:24 -0300

    update

--- src/Componentes/Sobre/index.tsx
+++ src/Componentes/Sobre/index.tsx
@@ -54,7 +54,7 @@ export default function Sobre() {
 
       <div className='flex flex-col ga-4 sm:w-1/2 w-full mt-24'>
         <p className='sm:items-start text-xl dark:text-white'>
-          Atualmente cursando Análise e Desenvolvimento de Sistemas na FIAP, com habilidades em desenvolvimento web, JavaScript, CSS, React.js, e git obtidas por meio de cursos na Alura. Busco desafios e oportunidades para aplicar meu conhecimento em projetos inovadores, colaborando com equipes talentosas para alcançar novos patamares na tecnologia. Sou comprometido, apaixonado e sempre em busca de aprendizado, pronto para contribuir com o sucesso de projetos e equipes de desenvolvimento. Vamos codar juntos!
+          Atualmente cursando Análise e Desenvolvimento de Sistemas na FIAP, com habilidades em desenvolvimento web, JavaScript, CSS, React.js, Next.js. Busco desafios e oportunidades para aplicar meu conhecimento em projetos inovadores, colaborando com equipes talentosas para alcançar novos patamares na tecnologia. Sou comprometido, apaixonado e sempre em busca de aprendizado, pronto para contribuir com o sucesso de projetos e equipes de desenvolvimento. Vamos codar juntos!
         </p>
         <BotaoContato />
       </div>
